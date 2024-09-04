2b8bfeebec3ab6ed9dc3bb2983953adc84abe6d0
GalloJunior <gallojunior@gmail.com>
2024-09-04 14:49:06 -0300

    Finalização da página de Receita e criação de serviço de usuário

---
+++ GCook/ViewModels/LoginVM.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace GCook.ViewModels;
+
+public class LoginVM
+{
+    [Display(Name = "Email ou Nome de Usuário", Prompt = "Email ou Nome de Usuário")]
+    [Required(ErrorMessage = "Por favor, informe seu email ou nome de usuário")]
+    public string Email { get; set; }
+    
+    [DataType(DataType.Password)]
+    [Display(Name = "Senha de Acesso", Prompt = "Senha de Acesso")]
+    [Required(ErrorMessage = "Por favor, informe sua senha")]
+    public string Senha { get; set; }
+
+    [Display(Name = "Manter Conectado?")]    
+    public bool Lembrar { get; set; } = false;
+    
+    public string UrlRetorno { get; set; }
+}
